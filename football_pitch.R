052ffc4c65e67c45d07ca7aa43d0348d5ca03b60
vilipo <vilipo@users.noreply.github.com>
2017-11-18 13:18:33 +0100

    plots_to_folder

--- football_pitch.R
+++ football_pitch.R
@@ -11,9 +11,14 @@ lapply(data_raw, table)
 
 one_game <- data_raw[gsm_id == gsm_id[2]][team == team[1], `:=` (location_x = -location_x, location_y = 420-location_y), team]
 
+player_plot <- function(player_chosen) {
+one_player <- data_raw[player == player_chosen]
+plot_data <- one_player
+
+title <- paste0(player_chosen, ' - ', one_player$team[1])
+
 ggplot() +
-  geom_rect(aes(xmin = -136, xmax = 136, ymin = 0 , ymax = 420), fill = '#336633', color = 'black', alpha = 0.8) +
-  geom_point(data = one_game, aes(location_x, location_y, color = icon, shape = bodyPart), size = 4) +
+  geom_rect(aes(xmin = -136, xmax = 136, ymin = 0 , ymax = 420), fill = 'white', color = 'black', alpha = 0.8) +
   geom_rect(aes(xmin = -15, xmax = 15, ymin = 0 , ymax = 2), fill = 'white', color = 'black', alpha = 0) +
   geom_rect(aes(xmin = -15, xmax = 15, ymin = 418 , ymax = 420), fill = 'white', color = 'black', alpha = 0) +
   geom_rect(aes(xmin = -37, xmax = 37, ymin = 0 , ymax = 22), fill = 'white', color = 'black', alpha = 0) + 
@@ -25,10 +30,17 @@ ggplot() +
   geom_point(aes(x = 0, y = 44), size=4, shape=20, color="black") +
   geom_point(aes(x = 0, y = 210), size=40, shape=1, color="black") +
   geom_point(aes(x = 0, y = 210), size=4, shape=20, color="black") +
+  geom_point(data = plot_data, aes(location_x, location_y, color = (icon=='goal'), shape = bodyPart), size = 4, shape = 19, alpha = 0.8) +
   theme(axis.line=element_blank(),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         axis.ticks=element_blank(),
         axis.title.x=element_blank(),
-        axis.title.y=element_blank())
+        axis.title.y=element_blank())+
+  ggtitle(title)
+
+ggsave(paste0('plots/'title, '.jpg'))
   
+}
+
+plot_list <- lapply(unique(data_raw$player)[10:20], player_plot)
